0a672f53f56cd7ddcf4e2d24f71c865049fb081f
StevenOyar <stvnoyaro@gmail.com>
2025-07-30 10:35:07 +0300

    added a simple calculator assigment

---
+++ assignment.py
@@ -0,0 +1,23 @@
+#asks the user to enter first and second numbers
+user_number1 = int(input("Enter the first number: "))
+user_number2= int(input("Enter the second number: "))
+
+
+#addding the two numbers
+addition = user_number1 + user_number2
+
+#multiplying the two numbers
+multiplication = user_number1 * user_number2
+
+#subtracting the two numbers
+subtraction = user_number1 - user_number2
+
+#dividing the two numbers
+division = user_number1 / user_number2
+
+
+#displaying the results
+print(f"Addittion: {addition}")
+print(f"Multiplication: {multiplication}")
+print(f"Subtraction: {subtraction}")
+print(f"Division: {division}")
